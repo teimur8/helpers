50c1b2376852784a56ccdb1eb59a4da33dd0b808
it06 <it06@kulanoil.kz>
2018-09-06 16:47:05 +0600

    ++ proxmox

--- php/mysql_created_updated_auto.sql
+++ php/mysql_created_updated_auto.sql
@@ -7,3 +7,39 @@ CREATE TABLE `img` (
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+create TEMPORARY table if not EXISTS temp21(
+	a varchar(255) null,
+	b varchar(255) null,
+	c varchar(255) null
+);
+
+insert into temp21(a,b,c)
+values (152560,	"MOBIL-1   0w-20	1л",	40),
+(152559,	"MOBIL-1   0w-20	4л",	120),
+(150690,	"MOBIL-1   0w-20	208л",	6240),
+(153790,	"M-1 ESP X2 0W20	1л",	30);
+
+
+INSERT INTO client_bonus_rules (
+name,targetId,maxDiscount,abcd,bonusPrice,bonusPriceRate,
+dateStart,dateEnd,isActive,toAllClient,bonusCount,product_code1c,
+rule_type,created_at,updated_at,deleted_at
+)
+SELECT
+  t.b,
+  p.prod_dev_code1c,
+  100, NULL, NULL,
+  t.c,
+  '2018-09-01 00:00:00',
+  '2018-12-31 00:00:00',
+  1,
+  1,
+  1,
+  p.prod_code1c,
+  'pvl', NOW(), NOW(), NULL
+FROM temp21 t
+LEFT JOIN 1c_products p ON p.prod_article = t.a
+WHERE p.prod_code1c IS NOT NULL
